5b4e28fca406be6da34dabdc9c1619b72769f095
yanruikun <1320784524@qq.com>
2017-05-09 08:54:57 +0800

    5555555555

--- jianshe/js/js.js
+++ jianshe/js/js.js
@@ -1,18 +1,23 @@
 window.onload = function(){
 	
+	$(".yrk_yin").hide();
 	$(".yrk_ss").click(function(){
 		$(".yrk_ss").hide();
-		$('.yrk_yin').show()
+		$('.yrk_yin').show();
+		$('.yrk_yin').animate({width: "300"}, 1000);
 	})
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	$("#bss").click(function(){
+		var search = $('#nei').val();
+		if(search.length > 0){
+			alert('没您所查找的内容');
+      $(".yrk_ss").show();
+      $(".yrk_yin").hide();
+      return false;
+      
+		}else{
+			alert('请输入要搜索的内容~')
+		}
+	})
 	
 }
